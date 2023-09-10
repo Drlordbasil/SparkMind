3fc91bde7b5d3caf77dae0bdd3a880dc35e6df22
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:45:21 -0400

    Auto-formatting code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,8 +1,9 @@
+import subprocess
+import os
 Sure! Here is the AI chatbot code that helps the user run the 'main.py' locally on their PC:
 
 ```python
-import os
-import subprocess
+
 
 class AIChatbot:
     def __init__(self):
@@ -19,7 +20,8 @@ class AIChatbot:
         return None
 
     def run_program(self):
-        process = subprocess.Popen(["python", self.program_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(
+            ["python", self.program_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         return stdout.decode(), stderr.decode()
 
@@ -28,39 +30,43 @@ class AIChatbot:
             dependencies_error = self.check_dependencies()
             if dependencies_error:
                 return "Error: Missing dependencies. Install the required libraries - requests, transformers, bs4"
-            
+
             if not os.path.exists(self.program_name):
                 return f"Error: '{self.program_name}' script not found."
-            
+
             stdout, stderr = self.run_program()
             output = stdout if stdout else stderr
             return output
-            
+
         return "Invalid input. Please enter 'run program' to execute the program."
 
     def start_chat(self):
         print("AI Chatbot: Hello! How can I assist you?")
-        
+
         while True:
             user_input = input("User: ")
             response = self.handle_user_input(user_input)
             print("AI Chatbot:", response)
-            
+
             if response == "Error: Missing dependencies. Install the required libraries - requests, transformers, bs4":
-                print("AI Chatbot: Please install the missing dependencies before running the program.")
+                print(
+                    "AI Chatbot: Please install the missing dependencies before running the program.")
                 break
-            
+
             if response.startswith("Error:"):
-                print("AI Chatbot: An error occurred while running the program. Please check the script and try again.")
+                print(
+                    "AI Chatbot: An error occurred while running the program. Please check the script and try again.")
                 break
-            
+
             if response.startswith("Invalid input"):
                 print("AI Chatbot: Please enter a valid command.")
                 continue
-            
-            confirmation = input("AI Chatbot: Do you want to run the program again? (Y/N): ")
+
+            confirmation = input(
+                "AI Chatbot: Do you want to run the program again? (Y/N): ")
             if confirmation.lower() != "y":
                 break
+
 
 if __name__ == "__main__":
     chatbot = AIChatbot()
