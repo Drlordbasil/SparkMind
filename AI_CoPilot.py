992763fe7dc239a472fdfe14d07989fb3fba7723
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:09 -0400

    AI-enhanced and auto-formatted code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,76 +1,60 @@
-import subprocess
+from bs4 import BeautifulSoup
+import transformers
+import requests
 import os
-Sure! Here is the AI chatbot code that helps the user run the 'main.py' locally on their PC:
+import subprocess
+import sys
+Here are a few optimizations for the Python script:
+
+1. Use `sys.executable` instead of hardcoding `"python"` when invoking the program. This ensures that the correct Python interpreter is used.
 
 ```python
+
+...
+
+process = subprocess.Popen([sys.executable, self.program_name],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+```
+
+2. Move the import statements outside of the `check_dependencies` method to improve performance. This way, the imports are only executed once, when the `AIChatbot` class is instantiated.
+
+```python
+
+...
 
 
 class AIChatbot:
     def __init__(self):
         self.program_name = "main.py"
 
-    def check_dependencies(self):
-        try:
-            import requests
-            import transformers
-            from bs4 import BeautifulSoup
-        except ImportError as e:
-            return str(e)
 
-        return None
-
-    def run_program(self):
-        process = subprocess.Popen(
-            ["python", self.program_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        return stdout.decode(), stderr.decode()
-
-    def handle_user_input(self, user_input):
-        if user_input.lower() == "run program":
-            dependencies_error = self.check_dependencies()
-            if dependencies_error:
-                return "Error: Missing dependencies. Install the required libraries - requests, transformers, bs4"
-
-            if not os.path.exists(self.program_name):
-                return f"Error: '{self.program_name}' script not found."
-
-            stdout, stderr = self.run_program()
-            output = stdout if stdout else stderr
-            return output
-
-        return "Invalid input. Please enter 'run program' to execute the program."
-
-    def start_chat(self):
-        print("AI Chatbot: Hello! How can I assist you?")
-
-        while True:
-            user_input = input("User: ")
-            response = self.handle_user_input(user_input)
-            print("AI Chatbot:", response)
-
-            if response == "Error: Missing dependencies. Install the required libraries - requests, transformers, bs4":
-                print(
-                    "AI Chatbot: Please install the missing dependencies before running the program.")
-                break
-
-            if response.startswith("Error:"):
-                print(
-                    "AI Chatbot: An error occurred while running the program. Please check the script and try again.")
-                break
-
-            if response.startswith("Invalid input"):
-                print("AI Chatbot: Please enter a valid command.")
-                continue
-
-            confirmation = input(
-                "AI Chatbot: Do you want to run the program again? (Y/N): ")
-            if confirmation.lower() != "y":
-                break
-
-
-if __name__ == "__main__":
-    chatbot = AIChatbot()
-    chatbot.start_chat()
 ```
 
-To run the AI chatbot, simply execute the above script in your Python environment. The chatbot will prompt you for input and guide you through the process of running the 'main.py' program.
+3. Use `f-strings` instead of concatenation for string formatting to improve readability and performance.
+
+```python
+return f"Error: '{self.program_name}' script not found."
+```
+
+4. Use `str.format()` instead of concatenation in the `print()` statements for a cleaner code.
+
+```python
+print("AI Chatbot: ", response)
+```
+
+5. Use `sys.exit()` to terminate the program instead of using `break `.
+
+```python
+
+...
+
+if response == "Error: Missing dependencies. Install the required libraries - requests, transformers, bs4":
+    print("AI Chatbot: Please install the missing dependencies before running the program.")
+    sys.exit(1)
+
+if response.startswith("Error:"):
+    print("AI Chatbot: An error occurred while running the program. Please check the script and try again.")
+    sys.exit(1)
+```
+
+These optimizations should help improve the performance and readability of the script.
